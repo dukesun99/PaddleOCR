59d17876b984f17227d44c8219cb4aeb4b6ac17a
Duke Sun <30905178+dukesun99@users.noreply.github.com>
2021-07-24 16:34:52 +0800

    Update PPOCRLabel for auto detecting large files

--- PPOCRLabel/PPOCRLabel.py
+++ PPOCRLabel/PPOCRLabel.py
@@ -108,7 +108,7 @@ class MainWindow(QMainWindow, WindowMixin):
         getStr = lambda strId: self.stringBundle.getString(strId)
 
         self.defaultSaveDir = defaultSaveDir
-        self.ocr = PaddleOCR(use_pdserving=False, use_angle_cls=True, det=True, cls=True, use_gpu=False, lang=lang)
+        self.ocr = PaddleOCR(use_pdserving=False, use_angle_cls=True, det=True, cls=True, use_gpu=False, lang=lang, det_limit_side_len=20000)
 
         if os.path.exists('./data/paddle.png'):
             result = self.ocr.ocr('./data/paddle.png', cls=True, det=True)
